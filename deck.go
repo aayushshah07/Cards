d703766fdc0d9997584485d7f9715e010d728d07
aayush <aayushshah719@gmail.com>
2021-09-19 14:44:53 +0530

    Shuffle cards Random Function

--- deck.go
+++ deck.go
@@ -3,7 +3,10 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 type deck []string
@@ -33,6 +36,29 @@ func (d deck) toString() string {
 func (d deck) saveToFile(fileName string) error {
 
 	return ioutil.WriteFile(fileName, []byte(d.toString()), 0666) //0666 is permission for anyone can write or read
+}
+
+func newDeckFromFile(fileName string) deck {
+	bs, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		fmt.Println("Error : ", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for i := range d {
+
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
 
 func (d deck) print() {
